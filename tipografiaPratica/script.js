be4eb0ac2dd781e7f34cd5d52906fd601d64229f
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2022-08-05 17:24:27 -0300

    finalizando ajuste tipografia

---
+++ tipografiaPratica/script.js
@@ -0,0 +1,5 @@
+let nome = document.querySelector('#nome')
+let telefone = document.querySelector('#telefone')
+let email = document.querySelector('#email')
+let mensagem = document.querySelector('#mensagem')
+
